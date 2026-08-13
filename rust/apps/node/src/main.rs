719817156a70c508857cf46d4056bf2dd558ea5c
mewov <stowhh@gmail.com>
2026-08-13 11:37:14 +0300

    style: update node/main

--- rust/apps/node/src/main.rs
+++ rust/apps/node/src/main.rs
@@ -11,15 +11,10 @@ async fn main() {
         tracing::error!(%err, "configuration");
         std::process::exit(1);
     });
-
-    if let Some(p) = config.pem_path.parent() {
-        let _ = std::fs::create_dir_all(p);
-    }
-    if let Some(p) = config.key_path.parent() {
-        let _ = std::fs::create_dir_all(p);
-    }
-
     tracing::info!("configuration");
+
+    if let Some(p) = config.pem_path.parent() { let _ = std::fs::create_dir_all(p); }
+    if let Some(p) = config.key_path.parent() { let _ = std::fs::create_dir_all(p); }
 
     rustls::crypto::ring::default_provider().install_default().unwrap_or_else(|err| {
         tracing::error!(?err, "rustls provider");
