4ae0181fddff233cee09832a26dbc01d5532c9b9
mewov <stowhh@gmail.com>
2026-08-13 10:56:18 +0300

    feat: add node configuration and logging

--- rust/apps/node/src/main.rs
+++ rust/apps/node/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+    tracing::info!("start node...");
+
+    dotenvy::dotenv().ok();
+    let config = config::Config::from_environment().unwrap_or_else(|err| {
+        tracing::error!(%err, "configuration");
+        std::process::exit(1);
+    });
+
+    if let Some(p) = config.pem_path.parent() {
+        let _ = std::fs::create_dir_all(p);
+    }
+    if let Some(p) = config.key_path.parent() {
+        let _ = std::fs::create_dir_all(p);
+    }
+
+    tracing::info!("configuration");
 }
