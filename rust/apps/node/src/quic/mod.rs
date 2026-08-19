3235daeab73fa95c33ca5afd9c797d0725876455
mewov <stowhh@gmail.com>
2026-08-19 22:51:37 +0300

    feat: add packets handler

--- rust/apps/node/src/quic/mod.rs
+++ rust/apps/node/src/quic/mod.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use ed25519_dalek::SigningKey;
+use proto::packets::types::{Packet, PacketKind};
 use quinn::{Connection, ServerConfig};
 use std::{net::SocketAddr, sync::Arc, time::Duration};
 
@@ -41,6 +42,20 @@ pub async fn connection(connection: Connection, write_timeout: Duration, read_ti
     let peer = peer::get_peer_cert_bytes(&connection).context("failed get peer identity")?;
     step.verify(&mut w, &peer, write_timeout, signing, VERSION).await?;
 
-    // ...
+    w.finish()?;
+
+    while let Ok((w, r)) = connection.accept_bi().await {
+        tokio::spawn(async move {
+            let mut frame = proto::packets::Frame::new(w, r, write_timeout, read_timeout);
+            while let Ok(p) = frame.recv().await {
+                match p.kind {
+                    PacketKind::Ping => {
+                        let _ = frame.send(&Packet::new_empty(PacketKind::Pong)).await;
+                    }
+                    _ => {}
+                }
+            }
+        });
+    }
     Ok(())
 }
