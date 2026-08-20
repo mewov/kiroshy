fee11976182737780d4e95c19de2c9e346e58453
mewov <stowhh@gmail.com>
2026-08-20 11:49:15 +0300

    feat: add client arguments parser and quic configurator

--- rust/apps/client/src/main.rs
+++ rust/apps/client/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Inspect {
+        #[arg(short, long)]
+        addr: String,
+        #[arg(short, long)]
+        public: String,
+    },
+}
+
+mod quic;
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    match args.command {
+        Command::Inspect { addr, public } => {}
+    }
 }
---
+++ rust/apps/client/src/quic/config.rs
@@ -0,0 +1,44 @@
+use quinn::ClientConfig;
+use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
+use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+use rustls::{DigitallySignedStruct, Error, SignatureScheme};
+use std::sync::Arc;
+
+#[derive(Debug)]
+struct SkipServerVerification;
+
+impl ServerCertVerifier for SkipServerVerification {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> Result<ServerCertVerified, Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(&self, _message: &[u8], _cert: &CertificateDer<'_>, _dss: &DigitallySignedStruct) -> Result<HandshakeSignatureValid, Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(&self, _message: &[u8], _cert: &CertificateDer<'_>, _dss: &DigitallySignedStruct) -> Result<HandshakeSignatureValid, Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        rustls::crypto::ring::default_provider().signature_verification_algorithms.supported_schemes()
+    }
+}
+
+pub fn create_insecure_client_config() -> Result<ClientConfig, Box<dyn std::error::Error>> {
+    let mut crypto = rustls::ClientConfig::builder()
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(SkipServerVerification))
+        .with_no_client_auth();
+
+    crypto.alpn_protocols = vec![b"node-v1".to_vec()];
+    let quic_config = quinn::crypto::rustls::QuicClientConfig::try_from(crypto)?;
+    Ok(ClientConfig::new(Arc::new(quic_config)))
+}
