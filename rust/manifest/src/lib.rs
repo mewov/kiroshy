a08b4d0f85474e11ddd439bad9bc0ae849954ca7
mewov <stowhh@gmail.com>
2026-08-21 13:06:19 +0300

    feat: add manifest structure

---
+++ rust/manifest/src/lib.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Manifest {
+    pub filename: String,
+    pub password_hash: [u8; 32],
+    pub blocks_length: u32,
+    pub payload: Vec<Block>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Block {
+    pub block_idx: u32,
+    pub block_id: [u8; 32],
+    pub nodes: Vec<NodeInfo>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NodeInfo {
+    pub addr: String,
+    pub public_key: [u8; 32],
+}
