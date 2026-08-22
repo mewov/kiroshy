725e2c8dccbe9334c6b1ee4ee90747678e774e3d
mewov <stowhh@gmail.com>
2026-08-22 13:44:13 +0300

    refactor: update manifest architecture

--- rust/manifest/src/lib.rs
+++ rust/manifest/src/lib.rs
@@ -1,23 +1,18 @@
-use serde::{Deserialize, Serialize};
-
-pub mod creator;
-pub mod parser;
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct Manifest {
     pub password_hash: [u8; 32],
     pub blocks_length: u32,
     pub payload: Vec<Block>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct Block {
     pub block_idx: u32,
     pub block_id: [u8; 32],
     pub nodes: Vec<NodeInfo>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct NodeInfo {
     pub addr: String,
     pub public_key: [u8; 32],
